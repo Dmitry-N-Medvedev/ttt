ac482ec9f1c36302feeab18ae7fcf829e356f0a8
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2020-10-31 00:01:54 +0100

    WIP: binding libt3 to ui
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

--- sources/front-end/src/components/Cell.svelte
+++ sources/front-end/src/components/Cell.svelte
@@ -9,7 +9,7 @@
 
   const handleClick = () => {
     dispatch('user:move', {
-      id: parseInt(cell.id, 10),
+      index: parseInt(cell.id, 10),
     });
   };
 </script>
@@ -26,7 +26,13 @@
     border-radius: 0.125vw;
   }
 
-  div:hover {
+  div:not(:empty) {
+    background-color: transparent;
+    pointer-events: none;
+    cursor: none;
+  }
+
+  div:empty:hover {
     background-color: var(--game-field-cell-background-color-hover);
   }
 </style>
--- sources/front-end/src/routes/index.svelte
+++ sources/front-end/src/routes/index.svelte
@@ -1,17 +1,93 @@
 <script>
+  import {
+    onMount,
+    onDestroy,
+  } from 'svelte';
+  import {
+    LibT3,
+  } from '@dmitry-n-medvedev/libt3/libt3.mjs';
+  import {
+    LibMatrix,
+  } from '@dmitry-n-medvedev/libt3/LibMatrix.mjs';
+  import {
+    XOF,
+  } from '@dmitry-n-medvedev/libt3/constants/XOF.mjs';
+  import {
+    BlockerStrategy,
+  } from '@dmitry-n-medvedev/libt3/strategies/BlockerStrategy.mjs';
+  import {
+    GameStates,
+  } from '@dmitry-n-medvedev/libt3/constants/GameStates.mjs';
+
+
   import Cell from '../components/Cell.svelte';
-  export let size = 3;
+  
+  const gameFieldSize = 3;
+  const libMatrix = new LibMatrix({
+    size: gameFieldSize,
+    emptyCellValue: XOF.F,
+  });
+  const playerConfig = Object.freeze({
+    name: 'Machine',
+    size: gameFieldSize,
+    strategy: new BlockerStrategy(),
+    symbols: {
+      human: XOF.X,
+      machine: XOF.O,
+      empty: XOF.F,
+    },
+    matrix: libMatrix,
+  });
+  const player = new LibT3(playerConfig);
 
   let cells = null;
-  let defaultValue = 0;
+  let defaultValue = '';
 
-  $: if (size) {
-    cells = new Array(Math.pow(size, 2)).fill({}).map((_, id) => ({ id, value: defaultValue }));
+  $: if (gameFieldSize) {
+    cells = new Array(Math.pow(gameFieldSize, 2)).fill({}).map((_, id) => ({ id, value: defaultValue }));
   }
 
-  const handleUserMove = ({ detail }) => {
-    console.debug('handleUserMove', detail);
+  const handleUserMove = ({ detail: { index } }) => {
+    libMatrix.set(index, XOF.X);
+
+    const moveResult = player.move();
+
+    console.debug('handleUserMove', moveResult);
+
+    if (moveResult.gameState === GameStates.WE_HAVE_A_WINNER) {
+      const { wonSymbol } = moveResult;
+      console.debug('WE_HAVE_A_WINNER', moveResult.vector, 'wonSymbol:', wonSymbol);
+    }
   };
+
+  const drawCell = (index, value) => {
+    console.debug(`drawCell: index == ${index}, value == ${value}`);
+
+    cells = cells.map((cell) => {
+      if (cell.id === index) {
+        return {
+          ...cell,
+          ...{
+            value: value,
+          },
+        }
+      }
+
+      return cell;
+    });
+  };
+
+  onMount(() => {
+    libMatrix.ondata = (index, value) => {
+      drawCell(index, value);
+    };
+  });
+
+  onDestroy(() => {
+    if (libMatrix) {
+      libMatrix.ondata = undefined;
+    }
+  });
 </script>
 
 <style>
